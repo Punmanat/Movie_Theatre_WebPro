a31dcd54a2b52904bf17294b6175dd42d841903c
Punmanat <60070053@kmitl.ac.th>
2019-02-24 01:44:32 +0700

    😂 almost success

--- components/saturday.js
+++ components/saturday.js
@@ -4,7 +4,7 @@ Vue.component('saturday',{
     style="width:100%; height:500px;background-color:white;"
   >
     <div
-      style="width:100%; height:10%;background: linear-gradient(to right, #99ff66 0%, #ff99cc 100%);"
+      style="width:100%; height:10%;background: linear-gradient(to right, #6699ff 0%, #ff99cc 100%);"
     >
       <p style="color:white;padding: 15px;" class="font">กรุงเทพและปริมณฑล</p>
     </div>
@@ -28,7 +28,7 @@ Vue.component('saturday',{
       </div>
     </div>
     <div
-      style="width:100%; height:10%;background: linear-gradient(to right, #99ff66 0%, #ff99cc 100%);"
+      style="width:100%; height:10%;background: linear-gradient(to right, #6699ff 0%, #ff99cc 100%);"
     >
       <p style="color:white;padding: 15px;" class="font">ภาคเหนือ</p>
     </div>
--- components/sunday.js
+++ components/sunday.js
@@ -4,7 +4,7 @@ Vue.component('sunday',{
     style="width:100%; height:500px;background-color:white;"
   >
     <div
-      style="width:100%; height:10%;background: linear-gradient(to right, #99ff66 0%, #ff6600 100%);"
+      style="width:100%; height:10%;background: linear-gradient(to right, #6699ff 0%, #ff99cc 100%);"
     >
       <p style="color:white;padding: 15px;" class="font">กรุงเทพและปริมณฑล</p>
     </div>
@@ -30,7 +30,7 @@ Vue.component('sunday',{
       </div>
     </div>
     <div
-      style="width:100%; height:10%;background: linear-gradient(to right, #99ff66 0%, #ff6600 100%);"
+      style="width:100%; height:10%;background: linear-gradient(to right, #6699ff 0%, #ff99cc 100%);"
     >
       <p style="color:white;padding: 15px;" class="font">ภาคเหนือ</p>
     </div>
