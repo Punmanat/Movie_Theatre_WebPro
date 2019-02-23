a6e0844558793e1789fe6df5de6382de219a3447
Punmanat <60070053@kmitl.ac.th>
2019-02-23 17:33:04 +0700

    ✂️ add button

--- movie.js
+++ movie.js
@@ -1,6 +1,6 @@
-Vue.component('movie', {
-    props:['movie'],
-    template : `
+Vue.component("movie", {
+  props: ["movie"],
+  template: `
     <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12" v-show="movie.showMovie">
     <div class="card mt-5" style="width: 18rem;">
         <a href="./movie_detail.html"><img class="card-img-top"
@@ -14,6 +14,7 @@ Vue.component('movie', {
             <p class="card-text font">นักแสดง : {{movie.director}}</p>
             <hr>
             <button @click="$emit('show-desc')" class="btn btn-success">แสดงเรื่องย่อ</button>
+            <a href="./theater.html" class="ml-4 btn btn-primary" style="color:white">ดูรอบฉาย</a>
             <p class="card-tex font" style="line-height: 1.6;" v-html="movie.synopsis" v-show="movie.showDesc"></p>
         </div>
     </div>
